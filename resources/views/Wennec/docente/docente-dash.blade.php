0a9ca72eedf4321c87db2e2f3959eb1199911abd
Efrain Andres Vergara Serrato <efrainvergara.udec@gmail.com>
2019-10-02 12:58:12 -0500

    Add other changes

--- resources/views/Wennec/docente/docente-dash.blade.php
+++ resources/views/Wennec/docente/docente-dash.blade.php
@@ -38,7 +38,7 @@
 ])
 @endcomponent
 <li class="nav-item">
-    <a href="http://127.0.0.1/wennec/wennec_aula_virtual/login/index.php" target="_blank" class="nav-link">
+    <a href="https://wennec.com/wennecaulas/" target="_blank" class="nav-link">
         <i class="fa fa-window-restore"></i>
         <span class="mini-click-non">Aula Virtual</span>
     </a>
